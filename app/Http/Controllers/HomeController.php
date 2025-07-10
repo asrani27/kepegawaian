266fc82b1125deb53cba0640677103bf1fce7660
asrani <asrani.27@gmail.com>
2025-07-10 11:09:39 +0800

    f

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -79,7 +79,7 @@ class HomeController extends Controller
     {
         //Sub Bidang Kepangkatan
         $pangkat = count(Pengajuan::where('jenis', 'kepangkatan')->where('status', 1)->get());
-        $diproses = count(Pengajuan::where('jenis', 'kepangkatan')->where('status', 1)->get());
+        $diproses = count(Pengajuan::where('jenis', 'kepangkatan')->where('status', 1)->where('verifikator', '!=', null)->get());
         $selesai = count(Pengajuan::where('jenis', 'kepangkatan')->where('status', 2)->get());
 
         $data = Pengajuan::where('jenis', 'kepangkatan')->where('status', '1')->get()->map(function ($item) {
